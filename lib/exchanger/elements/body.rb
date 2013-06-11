453ac405d54022199bb708a9ea3d5777460b1985
Julio Monteiro <julio@monteiro.eti.br>
2013-06-11 15:30:12 -0300

    No need to set tag_name, but we need to set the field_uri_namespace since there's only a text element

--- lib/exchanger/elements/body.rb
+++ lib/exchanger/elements/body.rb
@@ -3,15 +3,13 @@ module Exchanger
   #
   # http://msdn.microsoft.com/en-us/library/aa580339.aspx
   class Body < Element
+    self.field_uri_namespace = :'Body'
+
     element :text
 
     # Temp static tag attribute
     def tag_attributes
       { :body_type => "HTML" }
-    end
-
-    def tag_name
-      "Body"
     end
   end
 end
