9b7a8991dc51d987036b2691a3886cf6f0ea20eb
Evandro Dutra <evandro@jobscore.com>
2013-10-24 18:05:51 -0200

    feature: extended_property

---
+++ lib/exchanger/elements/extended_field_uri.rb
@@ -0,0 +1,16 @@
+module Exchanger
+  # The ExtendedFieldURI element identifies an extended MAPI property.
+  #
+  # http://msdn.microsoft.com/en-us/library/aa564843(v=exchg.140).aspx
+  class ExtendedFieldUri < Element
+
+    key :distinguished_property_set_id
+    key :property_name
+    key :property_type
+
+    def tag_name
+      "ExtendedFieldURI"
+    end
+
+  end
+end
