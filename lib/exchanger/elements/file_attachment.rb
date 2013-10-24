adcec3f763d11a25f72933dcb6af1ff0e8896ce8
Evandro Dutra <evandro@jobscore.com>
2013-10-24 18:06:00 -0200

    feature: attachments

---
+++ lib/exchanger/elements/file_attachment.rb
@@ -0,0 +1,18 @@
+module Exchanger
+  # The FileAttachment element represents a file that is attached to an item in the Exchange store.
+  #
+  # http://msdn.microsoft.com/en-us/library/aa580492(v=exchg.140).aspx
+  class FileAttachment < Element
+    element :attachment_id, :type => AttachmentId
+    element :name
+    element :content_type
+    element :content_id
+    element :content_location
+    element :size
+    element :last_modified_time
+    element :is_inline
+    element :is_contact_photo
+    element :content
+
+  end
+end
