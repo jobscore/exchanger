5b29d721b4b8f2aea0d80519e9f1099eadf05e80
Julio Monteiro <julio@monteiro.eti.br>
2013-11-12 22:10:19 -0200

    Additional "Restriction" so we can search for Extended Property

--- lib/exchanger/operations/find_item.rb
+++ lib/exchanger/operations/find_item.rb
@@ -4,7 +4,7 @@ module Exchanger
   # http://msdn.microsoft.com/en-us/library/aa566107.aspx
   class FindItem < Operation
     class Request < Operation::Request
-      attr_accessor :folder_id, :traversal, :base_shape, :email_address
+      attr_accessor :folder_id, :traversal, :base_shape, :email_address, :property_name, :property_value
 
       # Reset request options to defaults.
       def reset
@@ -35,6 +35,29 @@ module Exchanger
               xml.FindItem("xmlns" => NS["m"], "xmlns:t" => NS["t"], "Traversal" => traversal.to_s.camelize) do
                 xml.ItemShape do
                   xml.send "t:BaseShape", base_shape.to_s.camelize
+                  if property_name
+                    xml["t"].AdditionalProperties do
+                      xml["t"].ExtendedFieldURI(
+                        "PropertyName" => property_name.to_s,
+                        "DistinguishedPropertySetId" => "PublicStrings",
+                        "PropertyType" => "String"
+                      )
+                    end
+                  end
+                end
+                if property_name
+                  xml.Restriction do
+                    xml["t"].IsEqualTo do
+                      xml["t"].ExtendedFieldURI(
+                        "PropertyName" => property_name.to_s,
+                        "DistinguishedPropertySetId" => "PublicStrings",
+                        "PropertyType" => "String"
+                      )
+                      xml["t"].FieldURIOrConstant do
+                        xml["t"].Constant("Value" => property_value.to_s)
+                      end
+                    end
+                  end
                 end
                 xml.ParentFolderIds do
                   if folder_id.is_a?(Symbol)
